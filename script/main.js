c8accafd8e4bc992c885bc9db19aa81afb7e6f2f
Igor Gotovchik <gotovchik.igor@yandex.ru>
2024-04-19 11:42:28 +0300

    game logic

--- script/main.js
+++ script/main.js
@@ -22,6 +22,28 @@ function shuffle(arr) {
   return arr;
 }
 
+function createCard(nums, index) {
+  const cardNumber = document.createElement("p");
+  cardNumber.textContent = nums[index];
+  cardNumber.classList.add("card__number");
+  const card = document.createElement("div");
+  const cardFront = document.createElement("div");
+  const cardBack = document.createElement("div");
+  const cardQuestion = document.createElement("p");
+  cardQuestion.textContent = "?";
+  card.classList.add("card");
+  card.id = nums[index];
+  cardFront.classList.add("card__front");
+  cardQuestion.classList.add("card__question");
+  cardFront.append(cardQuestion);
+  cardBack.classList.add("card__back");
+  cardBack.append(cardNumber);
+  card.append(cardFront);
+  card.append(cardBack);
+  card.classList.add("flip");
+  return card;
+}
+
 function setDifficulty(count) {
   let difficulty = "easy";
   switch (count) {
@@ -34,47 +56,88 @@ function setDifficulty(count) {
   return difficulty;
 }
 
-function startGame(count = 8) {
-  cleanField();
-  const game = document.getElementById("game");
-  game.classList.add(setDifficulty(count));
-  const nums = shuffle(createNumbersArray(count));
-  for (let index = 0; index < nums.length; index++) {
-    const cardNumber = document.createElement("p");
-    cardNumber.textContent = nums[index];
-    cardNumber.classList.add("card__number");
-    const card = document.createElement("div");
-    const cardFront = document.createElement("div");
-    const cardBack = document.createElement("div");
-    card.classList.add("card");
-    card.id = nums[index];
-    cardFront.append(cardNumber);
-    cardFront.classList.add("card__front");
-    cardBack.classList.add("card__back");
-    cardBack.append(cardNumber);
-    card.append(cardFront);
-    card.append(cardBack);
-    game.append(card);
-    card.classList.add("flip");
-    setTimeout(() => {
-      card.classList.remove("flip");
-    }, 5000);
-  }
-
-  const cards = document.querySelectorAll(".card");
-  cards.forEach((card) => {
-    card.addEventListener("click", () => {
-      card.classList.toggle("flip");
-    });
-  });
-}
-
 function cleanField() {
   document.getElementById("game");
   game.innerHTML = "";
   game.classList.remove("easy");
   game.classList.remove("mid");
   game.classList.remove("hard");
+  game.classList.remove("game__result");
+}
+
+function startGame(count = 8) {
+  let firstCard = null;
+  let secondCard = null;
+  let clickable = false;
+
+  cleanField();
+  const game = document.getElementById("game");
+  game.classList.add(setDifficulty(count));
+  const nums = shuffle(createNumbersArray(count));
+  for (let index = 0; index < nums.length; index++) {
+    const card = createCard(nums, index);
+    game.append(card);
+    setTimeout(() => {
+      card.classList.remove("flip");
+      clickable = true;
+    }, 5000);
+  }
+
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card, index) => {
+    card.addEventListener("click", () => {
+      if (clickable && !card.classList.contains("success")) {
+        card.classList.add("flip");
+        if (firstCard === null) {
+          firstCard = index;
+        } else {
+          if (index != firstCard) {
+            secondCard = index;
+            clickable = false;
+          }
+        }
+
+        if (
+          firstCard !== null &&
+          secondCard !== null &&
+          firstCard !== secondCard
+        ) {
+          if (cards[firstCard].id === cards[secondCard].id) {
+            setTimeout(() => {
+              cards[firstCard].classList.add("success");
+              cards[secondCard].classList.add("success");
+
+              firstCard = null;
+              secondCard = null;
+              clickable = true;
+            }, 500);
+          } else {
+            setTimeout(() => {
+              cards[firstCard].classList.remove("flip");
+              cards[secondCard].classList.remove("flip");
+
+              firstCard = null;
+              secondCard = null;
+              clickable = true;
+            }, 500);
+          }
+        }
+        if (
+          Array.from(cards).every((card) => card.className.includes("flip"))
+        ) {
+          setTimeout(() => {
+            cleanField();
+            const congrat = document.createElement("p");
+            congrat.textContent =
+              "Поздравляю! Вы выиграли!\n Хотите заново? Выберите сложность.";
+            congrat.classList.add("game__congratulations");
+            game.classList.add("game__result");
+            game.append(congrat);
+          }, 2000);
+        }
+      }
+    });
+  });
 }
 
 (() => {
